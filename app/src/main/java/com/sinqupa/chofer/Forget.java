c4018a59d15b45875aa49aa3a0745aa66b9e0548
Angel Becerra <angel.becerracalle@hotmail.com>
2019-05-05 22:39:46 -0500

    Creacion de las Pantallas

---
+++ app/src/main/java/com/sinqupa/chofer/Forget.java
@@ -0,0 +1,13 @@
+package com.sinqupa.chofer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Forget extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forget);
+    }
+}
