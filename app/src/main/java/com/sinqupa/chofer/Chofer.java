cb0734663a1b5d843e5d20e47795caf1fb1117d1
Oscar <1520274@utp.edu.pe>
2019-06-07 00:40:41 -0500

    Cambios en el perfil Boton de detener no funciona

--- app/src/main/java/com/sinqupa/chofer/Chofer.java
+++ app/src/main/java/com/sinqupa/chofer/Chofer.java
@@ -2,12 +2,16 @@ package com.sinqupa.chofer;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -18,6 +22,12 @@ import android.widget.Gallery;
 import android.widget.TextView;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.sdsmdg.tastytoast.TastyToast;
+import com.sinqupa.chofer.service.LocationUpdatesService;
+import com.sinqupa.chofer.utility.Utility;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -26,10 +36,8 @@ public class Chofer extends Fragment {
     public static final String usuario="names";
     View vista;
     TextView txtUser;
+    Button btnIniciar, btnDetener, btnCerrar;
     private CircleImageView photoProfile;
-    private Button btnStorage;
-    private DatabaseReference fireBaseBD;
-    private static final int GALLERY_INTENT=1;
     Button btnCerrarSesion;
 
     @Override
@@ -42,34 +50,82 @@ public class Chofer extends Fragment {
         photoProfile=(CircleImageView)vista.findViewById(R.id.profile_image);
         String usuario =getActivity().getIntent().getStringExtra("names");
         txtUser.setText(usuario);
-        fireBaseBD= FirebaseDatabase.getInstance().getReference();
-        btnStorage=(Button)vista.findViewById(R.id.btnfoto);
 
-        btnStorage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(Intent.ACTION_PICK);
-                intent.setType("image/*");
-                startActivityForResult(intent, GALLERY_INTENT);
-
-            }
-        });
         btnCerrarSesion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 getActivity().finish();
             }
         });
+        //ESTO ES PARA LOS BOTONES DE
+
+        btnIniciar=vista.findViewById(R.id.btnStart);
+        btnDetener=vista.findViewById(R.id.btnCancelar);
+        btnCerrar=vista.findViewById(R.id.btnCerrarSesion);
+        //METODOS DE LOS BOTONES
+
+        btnIniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkPermissions()){
+                    requestLocationUpdates();
+                    TastyToast.makeText(getActivity().getApplicationContext(), "Aplicacion Iniciada", TastyToast.LENGTH_LONG, TastyToast.DEFAULT);
+                }
+            }
+        });
+
+
+
+        ///////////////////////////////////////////////////////////////////
         return vista;
+    }
+    private boolean checkPermissions() {
+        for(String permission : Utility.PERMISSIONS){
+            if(getActivity().checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED)
+                return false;
+        }
+        return true;
+    }
+
+    private void requestPermissions() {
+        List<String> remainingPermissions = new ArrayList<>();
+        for (String permission : Utility.PERMISSIONS) {
+            if (getActivity().checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED)
+                remainingPermissions.add(permission);
+        }
+        requestPermissions(remainingPermissions.toArray(new String[remainingPermissions.size()]), Utility.REQUEST_PERMISSIONS_REQUEST_CODE);
     }
 
     @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == GALLERY_INTENT && resultCode == Activity.RESULT_OK){
-            Uri uri=data.getData();
-            DatabaseReference filePath=fireBaseBD.child("fotos").child(uri.getLastPathSegment());
-
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == Utility.REQUEST_PERMISSIONS_REQUEST_CODE){
+            for(int i=0;i<grantResults.length;i++){
+                if(grantResults[i] != PackageManager.PERMISSION_GRANTED){
+                    if(shouldShowRequestPermissionRationale(permissions[i])){
+                        Snackbar snackbar = Snackbar.make(getView().findViewById(R.id.activity_welcome),Utility.PERMISSION_TEXT,Snackbar.LENGTH_INDEFINITE);
+                        snackbar.setActionTextColor(ContextCompat.getColor(getActivity(),R.color.colorDelete));
+                        snackbar.setAction(Utility.OK, new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                requestPermissions();
+                            }
+                        });
+                        snackbar.show();
+                    }
+                    return;
+                }
+            }
         }
+    }
+
+    private void requestLocationUpdates() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            getActivity().startForegroundService(new Intent(getActivity(), LocationUpdatesService.class));
+        }
+        getActivity().startService(new Intent(getActivity(), LocationUpdatesService.class));
+    }
+
+    private void removeLocationUpdates() {
+        getActivity().stopService(new Intent(getActivity(), LocationUpdatesService.class));
     }
 }
